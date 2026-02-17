fed20fcec76986673b974a36632908a1a2509ff1
Rushy <rushilshanmugam@gmail.com>
2026-02-17 12:22:06 -0500

    WOTD started

--- routes/words.js
+++ routes/words.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {readFile, writeFile} = require('fs').promises;
 
-router.get('/', (req, res)=>{      //  /user
+router.get('/', (req, res)=>{      
     res.send('Word Homepage');
 });
 
-router.get('/WOTD', (req, res)=>{   //  /user/new
-    res.send('Here is the Word of the Day!');
+router.get('/WOTD', (req, res)=>{   
+res.render('WOTD')
 });
 
 module.exports = router;
