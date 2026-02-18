bafe0e7d86fc88948f5a34f8b088aab5a345226f
Rushy <rushilshanmugam@gmail.com>
2026-02-18 12:18:10 -0500

    Finished WOTD

--- routes/words.js
+++ routes/words.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const {readFile, writeFile} = require('fs').promises;
+const {readFile, writeFile} = require('fs').promises; //destructuring
 
 router.get('/', (req, res)=>{      
     res.send('Word Homepage');
 });
 
-router.get('/WOTD', (req, res)=>{   
-res.render('WOTD')
+router.get('/WOTD', async (req, res)=>{   
+    let wordArray = await getWordFromDictionary();
+    let [word, part, definition] = wordArray;
+    res.render('WOTD',{word: word, part:part, definition:definition} )
 });
+
+
+
+
+
+let getWordFromDictionary = async ()=>{
+ try{
+    const data = await readFile('resources/allwords.txt' , 'utf8');
+    let lines = data.split('\n');
+    let randomNumber = parseInt(Math.random()*lines.length);
+    let randomLine = lines[randomNumber];
+    let wordArray = randomLine.split('\t');
+    console.log(wordArray);
+    return wordArray
+ }catch(err){
+    console.log("Error!")
+}
+};
 
 module.exports = router;
